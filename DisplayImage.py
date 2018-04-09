70faa3c26039d71607b799122d6a811a8cbd55c2
Macgeoffrey <macgeoffrey123@gmail.com>
2018-04-09 17:55:24 -0500

    Fixed Loading Bar to Work With PiTFT

--- DisplayImage.py
+++ DisplayImage.py
@@ -6,17 +6,14 @@ pygame.init()
 
 def LoadingScreen(x):
     lcd = pygame.display.set_mode((320, 240))
-    pygame.mouse.set_visible(False)
-
     my_image = pygame.image.load('PiWatchLogo.png')
 
     lcd.blit(my_image, (0, 0))
     pygame.display.update()
 
     progress = x
-    while progress <= 100 :
-        percentage = progress/100;
-        pygame.draw.rect(lcd, (255-(255*percentage),255*percentage,0), pygame.Rect(0,235,320*percentage,5))
+    while progress <= 320 :
+        pygame.draw.rect(lcd, (0,255,0), pygame.Rect(0,235,progress,5))
         pygame.display.update()
-        progress += 1
-        time.sleep(.05)
+        progress += 5
+        time.sleep(0.05)
--- PiWatchOS.py
+++ PiWatchOS.py
@@ -4,5 +4,8 @@ import time
 import DisplayImage
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
+pygame.mouse.set_visible(False)
 
 DisplayImage.LoadingScreen(0)
+
+
