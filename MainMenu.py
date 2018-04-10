496ed5fe4a7e655e95a3daa8968531d910ee6c8d
Macgeoffrey <macgeoffrey123@gmail.com>
2018-04-10 15:10:36 -0500

    Add files via upload

--- MainMenu.py
+++ MainMenu.py
@@ -4,6 +4,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
+lcd = pygame.display.set_mode((320, 240))
 
 #Creates Button Map
 button_map = {23:(255,0,0), 22:(0,255,0), 27:(0,0,255), 17:(0,0,0)}
@@ -19,7 +20,6 @@ GREY = (240,240,240)
 
 #Code to Draw The Menu
 def DrawMenu():
-    lcd = pygame.display.set_mode((320, 240))
     my_image = pygame.image.load('MenuImage.png')
 
     lcd.blit(my_image, (0, 0))
@@ -27,29 +27,44 @@ def DrawMenu():
     
 #Code to Check For Button Presses
 def MenuButtons():
+    DrawMenu()
+    MenuIsDrawn = 1
     font_medium = pygame.font.Font(None, 50)
- 
-    while True:
+
+    while MenuIsDrawn == 1:
         # Scan the buttons
         for (k,v) in button_map.items():
             if GPIO.input(k) == False:
                 if k == 27:
+                    lcd.fill((0,0,0))
                     text_surface = font_medium.render('Clock', True, WHITE)
                     rect = text_surface.get_rect(center=(160,120))
                     lcd.blit(text_surface, rect)
+                    pygame.display.update()
+                    sleep(1)
+                    MenuIsDrawn = 0
                 elif k == 23:
+                    lcd.fill((0,0,0)) 
                     text_surface = font_medium.render('Weather', True, WHITE)
                     rect = text_surface.get_rect(center=(160,120))
                     lcd.blit(text_surface, rect)
+                    pygame.display.update()
+                    sleep(1)
+                    MenuIsDrawn = 0
                 elif k == 22:
+                    lcd.fill((0,0,0))
                     text_surface = font_medium.render('Games', True, WHITE)
                     rect = text_surface.get_rect(center=(160,120))
                     lcd.blit(text_surface, rect)
+                    pygame.display.update()
+                    sleep(1)
+                    MenuIsDrawn = 0
                 else:
+                    lcd.fill((0,0,0))
                     text_surface = font_medium.render('Return', True, WHITE)
                     rect = text_surface.get_rect(center=(160,120))
                     lcd.blit(text_surface, rect)
-        sleep(0.1)
-
-DrawMenu()
-MenuButtons()
+                    pygame.display.update()
+                    sleep(1)
+                    MenuIsDrawn = 0
+        sleep(1)
